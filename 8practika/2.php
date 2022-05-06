d5d7b54f2f6856d27c1a96c8c0cdcca3c531d14c
MadiyarBaymurzinov <74169089+Mad1yar@users.noreply.github.com>
2022-05-06 10:59:25 +0600

    Add files via upload

---
+++ 8practika/2.php
@@ -0,0 +1,13 @@
+<?php
+    define(MY_AGE, 24);
+    echo $MY_AGE . '<br>';
+    $number = 5; //int
+    //$number = 45;
+    $num = -0,55; //float
+    $str = "Переменная"; //string
+    $bool = false; //boolean
+    $a = 0.5;
+    $b = "0.5";
+    echo $a + floatval($b);
+    echo $str . ':' .$number . '.Var 2: '. $num
+?>
---
+++ 8practika/2.zadanie 1.php
@@ -0,0 +1,13 @@
+<?php
+    $name = "Мадияр";
+    $surname = "Баймурзинов";
+    $date = 9;
+    $month = 5;
+    $year = 2002;
+    $age = 19;
+    $happy = 0.60;
+    echo "Имя: name, фамилия: surname";
+    echo "Дата рождения: $date $month $year";
+    echo "Возраст: $age";
+    echo "Уровень счатья: $happy";
+?>
